cd47d0d91290e2887ea38d07842630e2c8a36521
Jacky030716 <huan030716@gmail.com>
2024-11-13 21:43:20 +0800

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<script setup>
+import Navlink from "./Navlink.vue"
+import { Button } from "@/components/ui/button"
+import { Separator } from '@/components/ui/separator'
+import { navlinks } from "../constant/navlinks.js"
+
+const overviewLinks = navlinks.slice(0, 3)
+const resourcesLinks = navlinks.slice(3)
+</script>
+
+<template>
+  <nav class="px-5 py-8 flex flex-col justify-between gap-10 max-w-[250px] border h-screen rounded-xl shadow-secondary-600 shadow-lg">
+    <!-- Welcome Text -->
+    <div>
+      <h1 class="text-2xl text-primary font-bold">DriveHub</h1>
+      <p class="text-lg">Welcome Back, <span class="font-semibold">Teong Lee</span></p>
+    </div>
+
+    <!-- Navbar Body -->
+    <div class="flex flex-col h-full w-full gap-6">
+      <!-- Overview Section -->
+      <div class="flex flex-col gap-y-3.5">
+        <h4 class="text-secondary-600 font-bold text-xl">Overview</h4>
+        <ul class="flex flex-col items-start gap-2.5 text-secondary-500">
+          <Navlink 
+            v-for="(link, index) in overviewLinks" 
+            :key="index" 
+            :name="link.name" 
+            :href="link.href" 
+            :icon="link.icon" 
+          />
+        </ul>
+      </div>
+
+      <!-- Separator -->
+       <Separator />
+
+      <!-- Resources Section -->
+      <div class="flex flex-col gap-3.5">
+        <h4 class="text-secondary-600 font-bold text-xl">Resources</h4>
+        <ul class="flex flex-col items-start gap-2.5 text-secondary-500">
+          <Navlink 
+            v-for="(link, index) in resourcesLinks" 
+            :key="index" 
+            :name="link.name" 
+            :href="link.href" 
+            :icon="link.icon" 
+          />
+        </ul>
+      </div>
+    </div>
+
+    <Button>
+      Logout
+    </Button>
+  </nav>
+</template>
---
+++ src/constant/navlinks.js
@@ -0,0 +1,37 @@
+export const navlinks = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: '/src/assets/dashboard.svg'
+  },
+  {
+    name: 'Profile',
+    href: '/profile',
+    icon: '/src/assets/profile.svg'
+  },
+  {
+    name: 'Bookmark',
+    href: '/bookmark',
+    icon: '/src/assets/bookmark.svg'
+  },
+  {
+    name: "Hub",
+    href: "/hub",
+    icon: "/src/assets/hub.svg"
+  },
+  {
+    name: "Shared Resources",
+    href: "/shared",
+    icon: "/src/assets/shared.svg"
+  },
+  {
+    name: "Share",
+    href: "/share",
+    icon: "/src/assets/share.svg"
+  },
+  {
+    name: "Notifications",
+    href: "/notifications",
+    icon: "/src/assets/bell.svg"
+  }
+]
