2f0a9c44260e5d11e0b852ad07366c22615b9579
Lim Bo Yuan <limboyuanlby@gmail.com>
2024-11-17 00:52:04 +0800

    dynamic HubView title

--- src/views/HubView.vue
+++ src/views/HubView.vue
@@ -13,17 +13,19 @@
     isLoading: true
   })
 
+
+  let categoryName = ""
+
   onMounted(async () => {
   try {
     state.hub = (await axios({
       url: "/api/hubs",
     })).data;
     
-    // Find the category that matches the categoryId
-    const foundCategory = state.links.find(item => item.categoryId === props.categoryId);
-    if (foundCategory) {
-      result.value = foundCategory;
-    }
+    [state.hub] = state.hub.filter(item => item.categoryId === categoryId)
+    categoryName = state.hub.categoryName
+    
+    
   } catch (error) {
     console.log("Error fetching data:", error);
   } finally {
@@ -38,7 +40,7 @@
     <PulseLoader />
   </div>
   <div v-else class="bg-gray-100 shadow p-4 w-full flex flex-col ">
-    <h1 class="text-lg font-semibold text-center">Link Lists</h1>
+    <h1 class="text-lg font-semibold text-center">{{ categoryName }} Lists</h1>
     <div class="mt-2 max-h-[580px] overflow-y-auto">
       <LinkLists :categoryId="`${categoryId}`"/>
     </div>
